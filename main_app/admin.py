e5fc1b76f790538c3f05ec91cba8cc3e33ee3d4d
joyceyyuan <yueyuan@hotmail.com>
2022-09-01 00:21:16 -0700

    add walking model,display dog walking date&time

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 # import your models here
-from .models import Dog
+from .models import Dog, Walking
 
 # Register your models here
 admin.site.register(Dog)
+# register the new Walking Model 
+admin.site.register(Walking)
--- main_app/models.py
+++ main_app/models.py
@@ -13,3 +13,28 @@ class Dog(models.Model):
         # detail is referring to the name of the route
         # path('dogs/<int:dog_id>/', views.dogs_detail, name="detail"),
         return reverse('detail', kwargs={'dog_id': self.id})
+
+# Set up Field Choices
+SCHEDULE = (
+    ('M', 'Morning'),
+    ('A', 'Afternoon'),
+    ('N', 'Night'),
+)
+
+class Walking(models.Model):
+    # the first optional positional argument overrides the label
+    date = models.DateField('walking date')
+    schedule = models.CharField(
+        'walking schedule',
+        max_length=1, 
+        choices=SCHEDULE,  
+        default=SCHEDULE[0][0]  # 'M'
+    )
+
+    # create the dog_id FK (Foriegn Key)
+    # on_Delete is saying if we delete a dog, delete all of the walkings the dog has, so no walkings exist without a dog
+    dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # obtaining the human readable value of a Field.choice ('Morning')
+        return f"{self.get_schedule_display()} on {self.date}"
