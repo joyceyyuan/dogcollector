97cee53b4c52bec322ef709cb96d786d8fa1c272
joyceyyuan <yueyuan@hotmail.com>
2022-09-01 02:35:10 -0700

    register Treat in the admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 # import your models here
-from .models import Dog, Walking
+from .models import Dog, Walking, Treat
 
 # Register your models here
 admin.site.register(Dog)
-# register the new Walking Model 
 admin.site.register(Walking)
+admin.site.register(Treat)
