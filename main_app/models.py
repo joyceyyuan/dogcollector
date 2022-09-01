00cf447e9810bf9207071d3b22a3b30e6d0c94d1
joyceyyuan <yueyuan@hotmail.com>
2022-08-31 22:40:28 -0700

    add get_absolute_url to redirect after submitting creating cats form

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+# Import the reverse function
+from django.urls import reverse
 
 # Create your models here.
 class Dog(models.Model):
@@ -6,3 +8,8 @@ class Dog(models.Model):
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+    #this function will get called on a create or update on a Class Based View in order redirect the user
+    def get_absolute_url(self):
+        # detail is referring to the name of the route
+        # path('dogs/<int:dog_id>/', views.dogs_detail, name="detail"),
+        return reverse('detail', kwargs={'dog_id': self.id})
