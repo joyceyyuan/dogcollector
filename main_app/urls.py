f0b0fc66a2b89ff36191fef7e11d829ab0b1574c
joyceyyuan <yueyuan@hotmail.com>
2022-08-31 22:14:21 -0700

    add create cats functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('dogs/', views.dogs_index, name='index'),
     #route for the detail page
     path('dogs/<int:dog_id>/', views.dogs_detail, name='detail'),
+    # new route used to show a form and create a dog
+    path('dogs/create/', views.DogCreate.as_view(), name='dogs_create'),
 ]
